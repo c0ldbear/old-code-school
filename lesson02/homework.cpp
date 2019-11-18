0e5ca4192552e8e26db9c23c60db95efd735740a
Teddy <teddyjh@protonmail.com>
2019-11-18 21:12:21 +0100

    add a suggestion of a solution on hw01

--- lesson02/homework.cpp
+++ lesson02/homework.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 
 // Write a function that checks which integer is the biggest
+int checkInt(int number_a, int number_b)
+{
+    int result = number_a;
+    if (number_a < number_b)
+    {
+        result = number_b;
+    }
 
+    return result;
+}
 // Write a function that checks if a character is lowercase (use ascii table)
 
 // Write a function that makes a character uppercase ONLY IF it is lowercase
 
 int main()
 {
+
+    std::cout << checkInt(10,1) << std::endl;
+    std::cout << checkInt(3,51) << std::endl;
+    std::cout << checkInt(-5,-12) << std::endl;
+    std::cout << checkInt(123,123) << std::endl;
 
     return 0;
 }
