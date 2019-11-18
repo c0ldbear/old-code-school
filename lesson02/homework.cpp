aa7631253d69f69adaf8559729594f70217b174c
Teddy <teddyjh@protonmail.com>
2019-11-18 21:31:54 +0100

    add new function for checking lowercase characters
    We added a function which checks if a character is lowercase,
    and we started doing some simple test cases. We could add more
    test cases but it seems as that should not be necessary.

--- lesson02/homework.cpp
+++ lesson02/homework.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // Write a function that checks which integer is the biggest
-int checkInt(int number_a, int number_b)
+int CheckInt(int number_a, int number_b)
 {
     int result = number_a;
     if (number_a < number_b)
@@ -11,17 +11,41 @@ int checkInt(int number_a, int number_b)
 
     return result;
 }
+
+void TestCheckInt()
+{
+    std::cout << CheckInt(10,1) << std::endl;
+    std::cout << CheckInt(3,51) << std::endl;
+    std::cout << CheckInt(-5,-12) << std::endl;
+    std::cout << CheckInt(123,123) << std::endl;
+    std::cout << CheckInt('a','b') << std::endl;
+}
+
 // Write a function that checks if a character is lowercase (use ascii table)
+bool IsLowercase(char character)
+{
+    if (character >= 'a' && character <= 'z')
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void TestIsLowercase()
+{
+    std::cout << "a : " << IsLowercase('a') << std::endl;
+    std::cout << "z : " << IsLowercase('z') << std::endl;
+    std::cout << "A : " << IsLowercase('A') << std::endl;
+    std::cout << "Z : " << IsLowercase('Z') << std::endl;
+}
 
 // Write a function that makes a character uppercase ONLY IF it is lowercase
 
 int main()
 {
-
-    std::cout << checkInt(10,1) << std::endl;
-    std::cout << checkInt(3,51) << std::endl;
-    std::cout << checkInt(-5,-12) << std::endl;
-    std::cout << checkInt(123,123) << std::endl;
+    //TestCheckInt();
+    TestIsLowercase();
 
     return 0;
 }
