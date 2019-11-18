96640a42d2bff10b6a11de61f3937abbba0224dd
Teddy <teddyjh@protonmail.com>
2019-11-18 21:06:46 +0100

    add new file for the homework

---
+++ lesson02/homework.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Write a function that checks which integer is the biggest
+
+// Write a function that checks if a character is lowercase (use ascii table)
+
+// Write a function that makes a character uppercase ONLY IF it is lowercase
+
+int main()
+{
+
+    return 0;
+}
