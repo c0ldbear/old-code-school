daefcf7dd2b6d56fad95f9c988b732ec0646a32d
Teddy <teddyjh@protonmail.com>
2019-11-18 20:20:55 +0100

    start code for functions with declare and defines
    We have made a code now with a function: add(), where we declare
    and define the function in two steps, instead of one.
    
    The first line where the function is "mentioned", we have our
    declaration.
    
    After the main function, when we see add() again, there the
    function is defined.

---
+++ lesson02/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int add(int number_a, int number_b);
+
+int main()
+{
+
+    std::cout << add(5,10) << std::endl;
+
+    return 0;
+}
+
+int add(int number_a, int number_b)
+{
+    int result = number_a + number_b;
+
+    if (result > 10)
+    {
+        result += 1;
+    }
+
+    return result;
+}
