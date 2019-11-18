dd5fe16d816b8ebc32c88895840678b1c821c0c6
Teddy <teddyjh@protonmail.com>
2019-11-18 21:43:14 +0100

    remove comment and run all test cases for all functions
    We have removed the comments for all our test cases for our
    functions, and added a small print for showing which tests we are
    running.

--- lesson02/homework.cpp
+++ lesson02/homework.cpp
@@ -14,11 +14,13 @@ int CheckInt(int number_a, int number_b)
 
 void TestCheckInt()
 {
-    std::cout << CheckInt(10,1) << std::endl;
-    std::cout << CheckInt(3,51) << std::endl;
-    std::cout << CheckInt(-5,-12) << std::endl;
-    std::cout << CheckInt(123,123) << std::endl;
-    std::cout << CheckInt('a','b') << std::endl;
+    std::cout << "Test which integer is the biggest" << std::endl;
+    std::cout << "10 or 1: " << CheckInt(10,1) << std::endl;
+    std::cout << "3 or 51: " << CheckInt(3,51) << std::endl;
+    std::cout << "-5 or -12: " << CheckInt(-5,-12) << std::endl;
+    std::cout << "123 or 123: " << CheckInt(123,123) << std::endl;
+    std::cout << "a(97) or b(98): " << CheckInt('a','b') << std::endl;
+    std::cout << std::endl;
 }
 
 // Write a function that checks if a character is lowercase (use ascii table)
@@ -34,10 +36,12 @@ bool IsLowercase(char character)
 
 void TestIsLowercase()
 {
+    std::cout << "Test if we can find out if a character is lowercase" << std::endl;
     std::cout << "a : " << IsLowercase('a') << std::endl;
     std::cout << "z : " << IsLowercase('z') << std::endl;
     std::cout << "A : " << IsLowercase('A') << std::endl;
     std::cout << "Z : " << IsLowercase('Z') << std::endl;
+    std::cout << std::endl;
 }
 
 // Write a function that makes a character uppercase ONLY IF it is lowercase
@@ -53,6 +57,7 @@ char MakeUppercase(char character)
 
 void TestMakeUppercase()
 {
+    std::cout << "Test if we can make a lowercase to uppercase" << std::endl;
     std::cout << "a : " << MakeUppercase('a') << std::endl;
     std::cout << "b : " << MakeUppercase('b') << std::endl;
     std::cout << "z : " << MakeUppercase('z') << std::endl;
@@ -64,8 +69,8 @@ void TestMakeUppercase()
 
 int main()
 {
-    //TestCheckInt();
-    //TestIsLowercase();
+    TestCheckInt();
+    TestIsLowercase();
     TestMakeUppercase();
 
     return 0;
