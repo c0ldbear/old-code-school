27a8d6e2cd12059257040e346b12e44443971cae
Teddy <teddyjh@protonmail.com>
2019-11-18 20:37:01 +0100

    add damage logic
    Easy to see here that we have duplicate code, so we should be able
    to create a function for this part of the code.

--- lesson02/main.cpp
+++ lesson02/main.cpp
@@ -5,11 +5,25 @@ int main()
 
     int playerHealth = 100;
     int playerDamage = 8;
+    bool playerAlive = true;
 
     int enemyHealth = 50;
     int enemyDamage = 4;
+    bool enemyAlive = true;
 
+    playerHealth = playerHealth - enemyDamage;
+    if (playerHealth <= 0)
+    {
+        playerHealth = 0;
+        playerAlive = false;
+    }
 
+    enemyHealth = enemyHealth - playerDamage;
+    if (enemyHealth <= 0)
+    {
+        enemyHealth = 0;
+        enemyAlive = false;
+    }
 
     return 0;
 }
